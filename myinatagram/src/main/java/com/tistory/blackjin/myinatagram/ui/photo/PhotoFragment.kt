898dd717736cdc9ef418382130268bdb5cdb85d1
iwedding <dlwls5201@naver.com>
2019-11-26 16:10:40 +0900

    add GalleryUtil

--- myinatagram/src/main/java/com/tistory/blackjin/myinatagram/ui/photo/PhotoFragment.kt
+++ myinatagram/src/main/java/com/tistory/blackjin/myinatagram/ui/photo/PhotoFragment.kt
@@ -7,11 +7,20 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.tistory.blackjin.myinatagram.R
+import com.tistory.blackjin.myinatagram.builder.type.MediaType
 import com.tistory.blackjin.myinatagram.databinding.FragmentPhotoBinding
+import com.tistory.blackjin.myinatagram.model.Album
+import com.tistory.blackjin.myinatagram.util.GalleryUtil
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 
 class PhotoFragment : Fragment() {
 
     private lateinit var binding: FragmentPhotoBinding
+
+    private lateinit var disposable: Disposable
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +35,16 @@ class PhotoFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        loadMedia()
+    }
 
+    private fun loadMedia() {
+        disposable = GalleryUtil.getMedia(requireContext(), MediaType.IMAGE)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { albumList: List<Album> ->
+                Timber.d("albumList : $albumList")
+            }
     }
 
     companion object {
